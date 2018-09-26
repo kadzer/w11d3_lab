79c1770da90cfe2947e39c8d21c139e436cb20e1
kadzer <kadzer@gmail.com>
2018-09-26 14:29:59 +0100

    Adds methods to conference room for adding and removing guests, passes tests.

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -5,12 +5,14 @@ public class ConferenceRoom {
     private ArrayList<Guest> guests;
     private String name;
     private double dailyRate;
+    private boolean isFull;
 
-    public ConferenceRoom(int capacity, String name, double dailyRate) {
+    public ConferenceRoom(int capacity, String name, double dailyRate, boolean isFull) {
         this.capacity = capacity;
         this.guests = new ArrayList<>();
         this.name = name;
         this.dailyRate = dailyRate;
+        this.isFull = isFull;
     }
 
     public int getCapacity() {
@@ -23,5 +25,26 @@ public class ConferenceRoom {
 
     public double getDailyRate() {
         return dailyRate;
+    }
+
+    public boolean isFull() {
+        return isFull;
+    }
+
+    public int countGuests() {
+        return this.guests.size();
+    }
+
+    public void addGuest(Guest guest) {
+        if (!isFull) {
+            this.guests.add(guest);
+            if (countGuests() == getCapacity()) {
+                setFull(true);
+            }
+        }
+    }
+
+    public void setFull(boolean full) {
+        isFull = full;
     }
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -5,15 +5,17 @@ import static org.junit.Assert.assertEquals;
 
 public class ConferenceRoomTest {
     ConferenceRoom conferenceRoom;
+    Guest guest;
 
     @Before
     public void setUp() {
-        conferenceRoom = new ConferenceRoom(10, "Big Lebowski", 250.00);
+        conferenceRoom = new ConferenceRoom(2, "Big Lebowski", 250.00, false);
+        guest = new Guest("James Bond");
     }
 
     @Test
     public void returnsCapacity() {
-        assertEquals(10, conferenceRoom.getCapacity());
+        assertEquals(2, conferenceRoom.getCapacity());
     }
 
     @Test
@@ -24,5 +26,28 @@ public class ConferenceRoomTest {
     @Test
     public void returnsDailyRate() {
         assertEquals(250.00, conferenceRoom.getDailyRate(), 0.1);
+    }
+
+    @Test
+    public void canCountGuests() {
+        assertEquals(0, conferenceRoom.countGuests());
+    }
+
+    @Test
+    public void canAddGuest() {
+        conferenceRoom.addGuest(guest);
+        assertEquals(1, conferenceRoom.countGuests());
+    }
+
+    @Test
+    public void canGetWhetherFull() {
+        assertEquals(false, conferenceRoom.isFull());
+    }
+
+    @Test
+    public void isFull() {
+        conferenceRoom.addGuest(guest);
+        conferenceRoom.addGuest(guest);
+        assertEquals(true, conferenceRoom.isFull());
     }
 }
